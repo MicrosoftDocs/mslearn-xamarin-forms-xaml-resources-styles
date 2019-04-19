008c7474579f0f7694c1b5c8d2f41e27eae60e9f
Adam Patridge <patridge@gmail.com>
2019-04-19 16:52:35 -0600

    Import exercises.

---
+++ src/exercise3/final/TipCalculator/StandardTipPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+    public partial class StandardTipPage : ContentPage
+    {
+        public StandardTipPage()
+        {
+            InitializeComponent();
+            billInput.TextChanged += (s, e) => CalculateTip();
+        }
+
+        void CalculateTip()
+        {
+            double bill;
+
+            if (Double.TryParse(billInput.Text, out bill) && bill > 0)
+            {
+                double tip   = Math.Round(bill*0.15, 2);
+                double final = bill + tip;
+
+                tipOutput  .Text = tip  .ToString("C");
+                totalOutput.Text = final.ToString("C");
+            }
+        }
+
+        void OnLight(object sender, EventArgs e)
+        {
+            Resources["fgColor"] = Color.FromHex("#606060");
+            Resources["bgColor"] = Color.FromHex("#c0c0c0");
+        }
+
+        void OnDark(object sender, EventArgs e)
+        {
+            Resources["fgColor"] = Color.FromHex("#c0c0c0");
+            Resources["bgColor"] = Color.FromHex("#606060");
+        }
+
+        void GotoCustom(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CustomTipPage());
+        }
+    }
+}
+
---
+++ src/exercise3/start/TipCalculator/StandardTipPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+    public partial class StandardTipPage : ContentPage
+    {
+        public StandardTipPage()
+        {
+            InitializeComponent();
+            billInput.TextChanged += (s, e) => CalculateTip();
+        }
+
+        void CalculateTip()
+        {
+            double bill;
+
+            if (Double.TryParse(billInput.Text, out bill) && bill > 0)
+            {
+                double tip   = Math.Round(bill*0.15, 2);
+                double final = bill + tip;
+
+                tipOutput  .Text = tip  .ToString("C");
+                totalOutput.Text = final.ToString("C");
+            }
+        }
+
+        void OnLight(object sender, EventArgs e)
+        {
+            LayoutRoot.BackgroundColor = Color.Silver;
+
+            tipLabel   .TextColor = Color.Navy;
+            billLabel  .TextColor = Color.Navy;
+            totalLabel .TextColor = Color.Navy;
+            tipOutput  .TextColor = Color.Navy;
+            totalOutput.TextColor = Color.Navy;
+        }
+
+        void OnDark(object sender, EventArgs e)
+        {
+            LayoutRoot.BackgroundColor = Color.Navy;
+
+            tipLabel   .TextColor = Color.Silver;
+            billLabel  .TextColor = Color.Silver;
+            totalLabel .TextColor = Color.Silver;
+            tipOutput  .TextColor = Color.Silver;
+            totalOutput.TextColor = Color.Silver;
+        }
+
+        void GotoCustom(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CustomTipPage());
+        }
+    }
+}
+
